e64ac0db47e2ee6a05e0e59442be7d6513fd9c41
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-15 10:19:31 +0900

    JPABasic_8_OneToMany

---
+++ src/main/java/Test.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import config.MyPersistenceUnitInfo;
+import entity.Comment;
+import entity.Post;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.ManyToOne;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Map<String, String> props = new HashMap<>();
+		props.put("hibernate.show_sql","true");
+		props.put("hibernate.hbm2ddl.auto","create");	// create : drop & create, update : 있으면 안만들고 없으면 만든다.
+
+		EntityManagerFactory emf = new HibernatePersistenceProvider().createContainerEntityManagerFactory(
+				new MyPersistenceUnitInfo(), props
+		); // my persistence unit
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+		
+		
+		em.getTransaction().commit();  // 이 시점에 테이블에 반영한다.
+
+		
+
+		em.close();
+	}
+
+}
