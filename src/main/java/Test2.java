3157bd6f6dda1688f79d7265ec1b44b2e4549a18
tjdqls3607 <tjdqls3124@gmail.com>
2025-04-17 13:05:27 +0900

    12번 서브쿼리

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,5 +1,5 @@
+import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
@@ -7,11 +7,8 @@ import org.hibernate.jpa.HibernatePersistenceProvider;
 import config.MyPersistenceUnitInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.FetchType;
 
-//ManyToMany 양방향
-//teams,users, teams_users 3개의 테이블 생성확인
-// find
+// Group by, Order By
 public class Test2 {
 
 	public static void main(String[] args) {
@@ -27,8 +24,90 @@ public class Test2 {
 
 		em.getTransaction().begin();
 
-		// Test.java
-		// Test2는 맘대로 테스트 코드 작성
+////	 #1. group by
+		// 주문 건들을 주문일자 기준으로 묶어서 고객 주문 건수
+//		String jpql = 
+//			"""
+//			select o.orderDate, count(o.customer) as customerCount
+//				from Orders o  
+//			  group by o.orderDate
+//				
+//			""";
+//	
+//		em.createQuery(jpql, Object[].class)
+//			.getResultList()
+//			.forEach(objArray -> System.out.println(objArray[0] + ", " + objArray[1]));
+		
+		
+////	 #2. group by + having
+		// 주문 건들을 주문일자 기준으로 묶어서 고객 주문 건수 중 2 건 초과 건
+		// alias 로 having 조건 사용 X ( customerCount > 2 )
+//		String jpql = 
+//			"""
+//			select o.orderDate, count(o.customer) as customerCount
+//				from Orders o  
+//			  group by o.orderDate
+//			  having count(o.customer) > 2
+//			""";
+//	
+//		em.createQuery(jpql, Object[].class)
+//			.getResultList()
+//			.forEach(objArray -> System.out.println(objArray[0] + ", " + objArray[1]));
+		
+		
+////	 #3. group by + where
+		// 주문 건들을 주문일자 기준으로 묶어서 가져오되, 주문일자가 2024-03-11 보다 뒤 만
+		// alias 로 having 조건 사용 X ( customerCount > 2 )
+//		String jpql = 
+//			"""
+//			select o.orderDate, count(o.customer) as customerCount
+//				from Orders o  
+//			  where o.orderDate > :orderDate
+//			  group by o.orderDate
+//			""";
+//	
+//		em.createQuery(jpql, Object[].class)
+//			.setParameter("orderDate", LocalDate.of(2024, 3, 11))
+//			.getResultList()
+//			.forEach(objArray -> System.out.println(objArray[0] + ", " + objArray[1]));
+		
+		
+////	 #4. group by + where + order by
+		// 주문 건들을 주문일자 기준으로 묶어서 가져오되, 주문일자가 2024-03-11 보다 뒤 만
+		// alias 로 having 조건 사용 X ( customerCount > 2 )
+//		String jpql = 
+//			"""
+//			select o.orderDate, count(o.customer) as customerCount
+//				from Orders o  
+//			  where o.orderDate > :orderDate
+//			  group by o.orderDate
+//			  order by o.orderDate asc
+//			""";
+//	
+//		em.createQuery(jpql, Object[].class)
+//			.setParameter("orderDate", LocalDate.of(2024, 3, 11))
+//			.getResultList()
+//			.forEach(objArray -> System.out.println(objArray[0] + ", " + objArray[1]));
+		
+		
+////	 #5. group by + where + order by
+		// 주문 건 중 여성고객이 주문하고, 가격이 2000원 이상인 건들에 대해서, 주문일자별 제품 가격의 합을 구하되
+		// 주문 일자 내림차순으로 정렬
+		// 제품가격의 합 으로 정렬한다면 order by sum(p.price) desc
+		String jpql = 
+			"""
+			select o.orderDate, sum(p.price) as femaleOrderSum
+				from Orders o join o.customer c
+							  join o.product p
+			  where c.gender = 'f'
+			  and p.price >= 2000
+			  group by o.orderDate
+			  order by o.orderDate desc
+			""";
+	
+		em.createQuery(jpql, Object[].class)
+			.getResultList()
+			.forEach(objArray -> System.out.println(objArray[0] + ", " + objArray[1]));
 		
 		
 		em.getTransaction().commit();  // 이 시점에 테이블에 반영한다.
